9d85d72177cdfe4475c8084a3eebb9d4a4c52b31
Oded Welgreen <oded.welgreen@gmail.com>
2019-10-16 13:47:36 -0500

    starter

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-export class C {
-    private x = 10
-    getX = () => this.x;
-    setX = (newVal: number) => { this.x = newVal; }
-}
-
-export let x = new C();
-export let y = { ...{ some: "value" } }
+console.log(
+  "See tasdashis in your browser console: Typescript Webpack Starter Launched"
+);
