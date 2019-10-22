977324a7a2eddfc9676593d15dd962bb61704b1e
Oded Welgreen <oded.welgreen@gmail.com>
2019-10-21 23:17:54 -0500

    add lines + reset working

--- src/Initializer.ts
+++ src/Initializer.ts
@@ -29,7 +29,7 @@ export default class Initializer {
           ) as HTMLCanvasElement;
           canvas.style.letterSpacing = "10px";
           const ctx = canvas.getContext("2d");
-          ctx.font = `bold 20px ${font}`;
+          ctx.font = `bold 100px ${font}`;
           ctx.strokeStyle = "white";
           ctx.fillStyle = "white";
           ctx.textAlign = "center";
@@ -39,16 +39,16 @@ export default class Initializer {
             canvas.width / 2,
             canvas.height / 2
           );
-          // ctx.beginPath();
-          // ctx.moveTo(0, 40);
-          // ctx.lineTo(canvas.width, 40);
-          // ctx.moveTo(0, 50);
-          // ctx.lineTo(canvas.width, 50);
-          // ctx.moveTo(0, 220);
-          // ctx.lineTo(canvas.width, 220);
-          // ctx.moveTo(0, 230);
-          // ctx.lineTo(canvas.width, 230);
-          // ctx.stroke();
+          ctx.beginPath();
+          ctx.moveTo(0, 40);
+          ctx.lineTo(canvas.width, 40);
+          ctx.moveTo(0, 50);
+          ctx.lineTo(canvas.width, 50);
+          ctx.moveTo(0, 220);
+          ctx.lineTo(canvas.width, 220);
+          ctx.moveTo(0, 230);
+          ctx.lineTo(canvas.width, 230);
+          ctx.stroke();
 
           const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
           this.diff = [];
@@ -66,26 +66,48 @@ export default class Initializer {
   lastIndex: number;
   world: World;
   resolve: any;
+  first = true;
   init(world: World): Promise<void> {
     return new Promise((resolve, reject) => {
       this.initPromise.then(() => {
-        this.diff.sort(() => Math.random() - 0.5);
-        this.resolve = resolve;
-        this.world = world;
-        this.lastIndex = 0;
-        requestAnimationFrame(this.tick.bind(this));
+        for (let index = 0; index < this.diff.length; index++) {
+          world.set(this.diff[index], 1);
+        }
+        this.renderer.reset(this.options.model);
+        setImmediate(() => {
+          this.renderer.reset(this.options.model);
+          console.log("reset");
+          setTimeout(() => {
+            // if (this.first) {
+            this.renderer.render(world, this.diff);
+            console.log("render");
+            // this.first = false;
+            setTimeout(() => {
+              console.log("resolve");
+              resolve();
+            }, 1000);
+            // }
+          }, 0);
+        });
+
+        // this.diff.sort(() => Math.random() - 0.5);
+        // this.resolve = resolve;
+        // this.world = world;
+        // this.lastIndex = 0;
+
+        // requestAnimationFrame(this.tick.bind(this));
       });
     });
   }
-  tick() {
-    if (this.lastIndex === this.diff.length) {
-      this.resolve();
-      return;
-    }
+  // tick() {
+  //   if (this.lastIndex === this.diff.length) {
+  //     this.resolve();
+  //     return;
+  //   }
 
-    this.world.set(this.diff[this.lastIndex], 1);
-    this.renderer.render(this.world, [this.diff[this.lastIndex]]);
-    this.lastIndex++;
-    requestAnimationFrame(this.tick.bind(this));
-  }
+  //   this.world.set(this.diff[this.lastIndex], 1);
+  //   this.renderer.render(this.world, [this.diff[this.lastIndex]]);
+  //   this.lastIndex++;
+  //   requestAnimationFrame(this.tick.bind(this));
+  // }
 }
